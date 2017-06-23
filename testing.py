9017ac8acc67b7deb02beb94b4d89a2534541095
Robert Sandor <rsandor@sandors.com>
2017-06-23 17:15:51 +0000

    2nd Yet another testing.py

--- testing.py
+++ testing.py
@@ -2,20 +2,22 @@ import demoapp
 import unittest
 
 class FlaskTestCase(unittest.TestCase):
-  def setUp(self):
-   demoapp.app.config['TESTING'] = True
-   self.app = demoapp.app.test_client()
-   
-   def test_correct_http_response(self):
+
+    def setUp(self):
+        demoapp.app.config['TESTING'] = True
+        self.app = demoapp.app.test_client()
+
+    def test_correct_http_response(self):
        resp = self.app.get('/hello/world')
        self.assertEquals(resp.status_code, 200)
        
-      def test_correct_content(self):
+    def test_correct_content(self):
         resp = self.app.get('/hello/world')
         self.assertEquals(resp.data, '"Bob is the greatest guy!!!"\n')
-      def tearDown(self):
-          pass
+        
+    def tearDown(self):
+         pass
 
-      if __name__ == '__main__':
-        unittest.main()
+if __name__ == '__main__':
+    unittest.main()
 
